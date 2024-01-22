205302a99397f9a252f0c82259a9367ec3cdf8e3
ZwingliCaleb <calebzwingli@gmail.com>
2024-01-22 17:24:35 +0300

    Add Logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Header.scss'
+import logo from './src/assets/images/header_logo.png'
 function Header() {
   return (
     <div>
       <header className="App-header">
+        <img src = {logo} alt="Logo" className="App-logo" />
         <nav>
             <ul>
                 <li><a href="#Home">Home</a></li>
