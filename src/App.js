ccdad30095a7473e515cc4fdb7dc68b0f09c3092
ZwingliCaleb <calebzwingli@gmail.com>
2024-01-22 15:56:32 +0300

    Update my App.css and also added my hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
+import HeroSection from './components/HeroSection/HeroSection';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <HeroSection/>
     </div>
   );
 }
---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <section id="home">
+      <div className="hero-container">
+        <h1>Your Name</h1>
+        <p>Web Developer | Software Engineer</p>
+        {/* Add additional content or call-to-action buttons as needed */}
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
