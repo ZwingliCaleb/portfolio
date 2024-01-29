4d6ead0e2d203b6e1d51c24024299a3cec600546
ZwingliCaleb <calebzwingli@gmail.com>
2024-01-29 22:31:30 +0300

    new additions

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+
+    return (
+        <footer className-"footer-container">
+        <div className="social-icons">
+            
+        </div>
+    )
