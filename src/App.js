a8f7e1ef01f332afa1032a848537ad6a8c3177f4
ZwingliCaleb <calebzwingli@gmail.com>
2024-02-12 23:18:17 +0300

    rearrangeee

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Portfolio from './pages/Portfolio/Portfolio';
@@ -10,12 +10,12 @@ function App() {
   return (
     <Router>
       <div>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
+        <Header/>
+          <Routes>
+            <Route path="/" exact render={() => <Home />} />
+            <Route path="/portfolio" render={() => <Portfolio/>} />
+            <Route path="/contact" render={() => <Contact/>} />
+          </Routes>
       </div>
     </Router>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,1 +1,17 @@
 import React from 'react';
+import HeroSection from '../../components/HeroSection/HeroSection';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import  './Home.scss';
+
+const Home = () => {
+    return (
+        <>
+            <Header/>
+            <HeroSection/>
+            <Footer/>
+        </>
+    );
+};
+
+export default Home;
