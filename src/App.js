df8c7f7ff72b7cc82f0972b33fb1b9624c2d9ee0
ZwingliCaleb <calebzwingli@gmail.com>
2024-02-14 22:52:55 +0300

    Update Babel in package.json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
 import HeroSection from './components/HeroSection/HeroSection';
 import Footer from './components/Footer/Footer';
 
-function App() {
+const App = () => {
   return (
     <div>
       <Header />
