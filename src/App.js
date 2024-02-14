bc47f9ac517f67c26b6c7858e0effbbd33ed4f40
ZwingliCaleb <calebzwingli@gmail.com>
2024-02-14 22:58:19 +0300

    Steps at a time deletion of a few  components awaiting rewriting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import Portfolio from './components/Portfolio/Portfolio';
-import Contact from './components/Contact/Contact';
 import HeroSection from './components/HeroSection/HeroSection';
 import Footer from './components/Footer/Footer';
 
@@ -10,8 +8,6 @@ const App = () => {
     <div>
       <Header />
       <HeroSection/>
-      <Portfolio />
-      <Contact />
       <Footer/>
     </div>
   );
