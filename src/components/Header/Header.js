b09d21384e495886f2d99882d07640cce88dc679
ZwingliCaleb <calebzwingli@gmail.com>
2024-02-18 10:07:47 +0300

    install react scroll and start implementation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Header.scss';
 import logo from '../../assets/images/logo2.png';
+import { Link } from 'react-scroll';
 function Header() {
   return (
     <div>
       <header className="App-header">
         <nav>
             <ul>
-                <img src={logo} alt="logo" className="logo" />
-                <li><a href="#Home">Home</a></li>
-                <li><a href="#About">About</a></li>
-                <li><a href="#Portfolio">Portfolio</a></li>
-                <li><a href="#Contact">Contact</a></li>
+            <Link to="Home" smooth={true} duration={500}><img src={logo} alt="logo" className="logo" /></Link>
+            <Link to="Home" smooth={true} duration={500}>Home</Link>
+            <Link to="About" smooth={true} duration={500}>About</Link>
+            <Link to="Portfolio" smooth={true} duration={500}>Portfolio</Link>
+            <Link to="Contact" smooth={true} duration={500}>Contact</Link>  
             </ul>
         </nav>
       </header>
