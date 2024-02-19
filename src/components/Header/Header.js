f046bacbd50315f0f8be432f0d5e8357c018d5e2
ZwingliCaleb <calebzwingli@gmail.com>
2024-02-19 19:03:36 +0300

    new change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,11 @@ function Header() {
       <header className="header">
         <nav>
             <ul>
-            <Link to="img" smooth={true} duration={500}><img src={logo} alt="logo" className="logo" /></Link>
-            <Link to="Home" smooth={true} duration={500}>Home</Link>
-            <Link to="About" smooth={true} duration={500}>About</Link>
-            <Link to="Portfolio" smooth={true} duration={500}>Portfolio</Link>
-            <Link to="Contact" smooth={true} duration={500}>Contact</Link>  
+            <li><Link to="img" smooth={true} duration={500}><img src={logo} alt="logo" className="logo" /></Link></li>
+            <li><Link to="Home" smooth={true} duration={500}>Home</Link></li>
+            <li><Link to="About" smooth={true} duration={500}>About</Link></li>
+            <li><Link to="Portfolio" smooth={true} duration={500}>Portfolio</Link></li>
+            <li><Link to="Contact" smooth={true} duration={500}>Contact</Link></li>  
             </ul>
         </nav>
       </header>
