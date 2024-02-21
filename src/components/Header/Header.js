85ac9f7e9a2befd8749a54283ba6e9cfd034db41
ZwingliCaleb <calebzwingli@gmail.com>
2024-02-22 01:33:05 +0300

    new changes...remove logo.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Header.scss';
-import logo from '../../assets/images/logo2.png';
 import { Link } from 'react-scroll';
 function Header() {
   return (
@@ -8,7 +7,6 @@ function Header() {
       <header className="header">
         <nav>
             <ul>
-            <li><Link to="img" smooth={true} duration={500}><img src={logo} alt="logo" className="logo"/></Link></li>
             <li><Link to="Home" smooth={true} duration={500}>Home</Link></li>
             <li><Link to="About" smooth={true} duration={500}>About</Link></li>
             <li><Link to="Portfolio" smooth={true} duration={500}>Portfolio</Link></li>
--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -4,10 +4,12 @@ import './HeroSection.scss';
 const HeroSection = () => {
   return (
     <div className="hero-container" id="Home">
-      <h3>Hello,</h3>
-      <h1>I AM ZWINGLI.</h1>
-      <h2>Software Engineer | Frontend Developer</h2>
-      <h3>Bit by bit, byte by byte: Crafting digital masterpieces.</h3>
+      <div clasName="hero-box">
+        <h3>Hello,</h3>
+        <h1>I AM ZWINGLI.</h1>
+        <h2>Software Engineer | Frontend Developer</h2>
+        <h3>Bit by bit, byte by byte: Crafting digital masterpieces.</h3>
+      </div>
     </div>
   );
 };
