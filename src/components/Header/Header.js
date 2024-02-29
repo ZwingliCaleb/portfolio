e3c14b1ab3ecba6dda4e320e8eabd5368b061066
ZwingliCaleb <calebzwingli@gmail.com>
2024-02-29 14:49:31 +0300

    implementing burgermenu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
 import { Link } from 'react-scroll';
+import { slide as Menu } from 'react-burger-menu';
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div>
       <header className="Header">
+        <div className="BurgerMenu" onClick={handleMenuClick}>
+          ☰
+        </div>
         <nav className="nav-container">
             <ul>
               <li><Link to="Home" smooth={true} duration={500}>Home</Link></li>
@@ -14,8 +23,15 @@ function Header() {
             </ul>
         </nav>
       </header>
+      <Menu isOpen={menuOpen} onStateChange={(state) => setMenuOpen(state.isOpen)}>
+        <Link to="Home" smooth={true} duration={500} onClick={handleMenuClick}></Link>
+        <Link to="About" smooth={true} duration={500} onClick={handleMenuClick}></Link>
+        <Link to="Portfolio" smooth={true} duration={500} onClick={handleMenuClick}></Link>
+        <Link to="Contact" smooth={true} duration={500} onClick={handleMenuClick}></Link>
+      </Menu>
+
     </div>
-  )
+  );
 }
 
 export default Header
