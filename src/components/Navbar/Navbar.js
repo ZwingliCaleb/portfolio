c7fe6f87709f96029e6ca712ecd0f0234759f3b8
ZwingliCaleb <calebzwingli@gmail.com>
2024-03-08 13:43:06 +0300

    react-scroll acting up

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,27 @@
 import { useRef } from 'react';
 import './Navbar.scss';
-//import { Link } from 'react-scroll';
+import { Link, animateScroll as scroll } from 'react-scroll';
 import { FaBars, FaTimes } from 'react-icons/fa';
 function Navbar() {
   const navRef = useRef();
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive_nav");
+  };
+
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+    showNavbar();
   }
 
   return (
     <div>
       <header>
-        <h3>Zwingli</h3>
+        <h3 onClick={scrollToTop}>Zwingli</h3>
         <nav ref={navRef}>
-              <a href="/#">Home</a>
-              <a href="/#">About</a>
-              <a href="/#">Portfolio</a>
-              <a href="/#">Contact</a>
+              <Link to="Home" smooth={true} duration={500}>Home</Link>
+              <Link to="About" smooth={true} duration={500 }>About</Link>
+              <Link to="Portfolio" smooth={true} duration={500}>Portfolio</Link>
+              <Link to="Contact" smooth={true} duration={500}>Contact</Link>
               <button className="nav-btn nav-close-btn" onClick={showNavbar}>
                 <FaTimes />
               </button>
