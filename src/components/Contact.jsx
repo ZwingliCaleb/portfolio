9f0fdce7247ee7980820ee1c443fdea2344b50e9
ZwingliCaleb <calebzwingli@gmail.com>
2024-06-10 21:29:49 +0300

    new changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,15 +3,12 @@ import ContactForm from "./ContactForm";
 
 const Contact = () => {
 	return (
-		<div
-			className="flex justify-center items-center min-h-screen md:min-h-auto bg-blue-950 text-white w-full py-20"
-			id="Contact"
-		>
+		<section className="flex flex-wrap justify-center items-center min-h-screen md:min-h-auto bg-blue-950 text-white w-full py-20" id="Contact">
+			<h1 className="text-5xl mb-6">Get in touch!</h1>
 			<div className="max-w-md w-full p-8 bg-gray-800 shadow-md rounded-md text-center">
-				<h1 className="text-5xl mb-6">Get in touch!</h1>
 				<ContactForm />
 			</div>
-		</div>
+		</section>
 	);
 };
 
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+//import {Input} from "@nextui-org/react";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
